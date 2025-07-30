229c70270ab2c558324f734a84cbe44f06a253d1
palexdev <alessandro.parisi406@gmail.com>
2025-07-30 21:56:19 +0200

    :wrench: Add JavaFX with plugin and make module transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module HotSwapFX {
     requires java.management;
-    requires javafx.graphics;
+    requires transitive javafx.graphics;
     requires org.jooq.joor;
     requires directory.watcher;
     requires org.tinylog.api;
