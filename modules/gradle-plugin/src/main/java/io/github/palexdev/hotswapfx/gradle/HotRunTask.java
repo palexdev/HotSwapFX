71a817739b11a93c7312be60a12ecaeba96167c7
palexdev <alessandro.parisi406@gmail.com>
2026-03-21 19:01:44 +0100

    :recycle: (gradle-plugin) Disable caching for HotRunTask
    (cherry picked from commit f5555fdfb589a3e100ad93b00a6a552e7dc3c2c1)

--- modules/gradle-plugin/src/main/java/io/github/palexdev/hotswapfx/gradle/HotRunTask.java
+++ modules/gradle-plugin/src/main/java/io/github/palexdev/hotswapfx/gradle/HotRunTask.java
@@ -2,8 +2,10 @@ package io.github.palexdev.hotswapfx.gradle;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.work.DisableCachingByDefault;
 
 /// Dummy task to execute the `run` task in hot swap mode.
+@DisableCachingByDefault
 public class HotRunTask extends DefaultTask {
 
     //================================================================================
