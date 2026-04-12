5b6e3cc0f65859d4af1cb31eb25218d3a5d2f98d
palexdev <alessandro.parisi406@gmail.com>
2026-04-12 20:41:36 +0200

    Update dev scripts

--- scripts/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call jbang export fatjar --force --output=binaries\UpdateChangelogs.jar .\src\UpdateChangelogs.java
-call jbang export fatjar --force --output=binaries\Release.jar .\src\Release.java
